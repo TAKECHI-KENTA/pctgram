632e927ad36d6adf51ea7299b10dccdb09b848b5
TAKECHI Kenta <mobile.kenta.takechi@gmail.com>
2022-08-28 00:56:54 +0000

    ログアウト機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
       redirect_to root_path, success: '登録が完了しました'
     else
       flash.now[:danger] = "登録に失敗しました"
+      #binding.pry
       render :new
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'sessions/new'
   get 'pages/index'
   root 'pages#index'
-end
-
-Rails.application.routes.draw do
-  root 'pages#index'
   get 'pages/help'
-  
-  resources :users
-end
 
-Rails.application.routes.draw do
-  get 'users/new'
-  root 'pages#index'
-  get 'pages/link'
+  resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
