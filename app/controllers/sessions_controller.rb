8d3afd60db7d877e225836c085c5e1381f76a530
TAKECHI Kenta <mobile.kenta.takechi@gmail.com>
2022-08-29 02:49:05 +0000

    Rails5課題修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,11 @@ class SessionsController < ApplicationController
   
   def create
     #user = User.find_by(email: params[:session][:email])
-    user = User.find_by(email_params)  #ストロングパラメーターに変更
+    #user = User.find_by(email_params)  #ストロングパラメーターに変更
+    user = User.find_by(user_params[:email]) #ストロングパラメーターを一つに集約
     #if user && user.authenticate(params[:session][:password])
-    if user && user.authenticate(password_params[:password])  #ストロングパラメーターに変更
+    #if user && user.authenticate(password_params[:password])  #ストロングパラメーターに変更
+    if user && user.authenticate(user_params[:password])  #ストロングパラメーターを一つに集約
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
@@ -26,13 +28,16 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
   end
   
-  def email_params
-    params.require(:session).permit(:email)
-  end
+#  def email_params   ストロングパラメーターを一つに集約
+ #   params.require(:session).permit(:email)
+  #end
   
-  def password_params
-    params.require(:session).permit(:password)
+  #def password_params　　ストロングパラメーターを一つに集約
+    #params.require(:session).permit(:password)
   
+  def user_params
+    params.require(:session).permit(:email, :password)
+    
   def log_out
     session.delete(:user_id)
     @current_user = nil
