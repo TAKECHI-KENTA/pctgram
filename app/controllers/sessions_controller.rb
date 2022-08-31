d3be4364b8d645f1a79aae6cc2790cabe739badf
TAKECHI Kenta <mobile.kenta.takechi@gmail.com>
2022-08-31 03:16:32 +0000

    ログイン・ログアウトの整理(未了)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,15 @@
 class SessionsController < ApplicationController
+
   def new
   end
   
   def create
-    #user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email])
     #user = User.find_by(email_params)  #ストロングパラメーターに変更
-    user = User.find_by(user_params[:email]) #ストロングパラメーターを一つに集約
-    #if user && user.authenticate(params[:session][:password])
-    #if user && user.authenticate(password_params[:password])  #ストロングパラメーターに変更
-    if user && user.authenticate(user_params[:password])  #ストロングパラメーターを一つに集約
+    #user = User.find_by(user_params[:email]) #ストロングパラメーターを一つに集約
+    if user && user.authenticate(params[:session][:password])
+    #if user && user.authenticate(password_params[:password]) #ストロングパラメーターに変更
+    #if user && user.authenticate(user_params[:password])  #ストロングパラメーターを一つに集約
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
@@ -19,26 +20,30 @@ class SessionsController < ApplicationController
   
   def destroy
     log_out
-    redirect_to root_rul, info: 'ログアウトしました'
+    redirect_to root_url, info: 'ログアウトしました'
   end
   
+    protect_from_forgery #追記
   
   private
   def log_in(user)
     session[:user_id] = user.id
   end
   
-#  def email_params   ストロングパラメーターを一つに集約
- #   params.require(:session).permit(:email)
-  #end
+  def email_params   #ストロングパラメーター
+    params.require(:session).permit(:email)
+  end
   
-  #def password_params　　ストロングパラメーターを一つに集約
-    #params.require(:session).permit(:password)
+  def password_params　　#ストロングパラメーター
+    params.require(:session).permit(:password)
+  end 
   
-  def user_params
-    params.require(:session).permit(:email, :password)
+ # def user_params  #ストロングパラメーターを一つに集約
+  #  params.require(:session).permit(:email, :password)
+  #end 
     
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end 
 end
