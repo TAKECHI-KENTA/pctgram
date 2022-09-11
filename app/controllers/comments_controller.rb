af4473f0cb29c1d3a3ae557ed5ccc4dd94c370c3
TAKECHI Kenta <mobile.kenta.takechi@gmail.com>
2022-09-11 10:50:34 +0000

    Rails07 課題コメント実装(完成)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,17 +4,16 @@ class CommentsController < ApplicationController
   end
   
   def new
-    @comment = Comment.new
+    @comment = Comment.new  #commentのnew.erb.htmlで入力された情報を格納する
+    @topic_id = params[:topic_id] #commentのnew.erb.htmlでtopic_idを引き継ぐために情報を取得する
   end
   
   def create
-    @topic = Topic.find(params[:topic_id])
-    @comment = current_user.comments.new(comment_params)
+    #@topic = Topic.find(params[:topic_id])
+    comment = current_user.comments.new(comment_params)
     #comment.topic_id = topic.id
     
-
-    
-    if @comment.save
+    if comment.save
       redirect_to topics_path, success: 'コメントに成功しました'
     else
       flash.now[:danger] = "コメントに失敗しました"
